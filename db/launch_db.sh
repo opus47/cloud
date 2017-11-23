ca1386abd3c0bb13cbe6c6e668f80ec95d34deef
Ryan Goodfellow <rgoodfel@isi.edu>
2017-11-23 01:23:35 -0800

    db docker

---
+++ db/launch_db.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker run -d --name opus47-db --rm  opus47/db:latest
---
+++ db/seed-db.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker exec opus47-db mongoimport --db opus47 --jsonArray --collection keys --file /data47/keys.json
+docker exec opus47-db mongoimport --db opus47 --jsonArray --collection composers --file /data47/composers.json
+docker exec opus47-db mongoimport --db opus47 --jsonArray --collection pieces --file /data47/brahms-pieces.json
+docker exec opus47-db mongoimport --db opus47 --jsonArray --collection musicians --file /data47/musicians.json
+docker exec opus47-db mongoimport --db opus47 --jsonArray --collection performances --file /data47/olyfest-perf.json
+docker exec opus47-db mongoimport --db opus47 --jsonArray --collection recordings --file /data47/olyfest-rec.json
+docker exec opus47-db mongoimport --db opus47 --jsonArray --collection parts --file /data47/parts.json
