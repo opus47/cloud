aefe91765b4145d6253a3b8bbf414633af80079f
Ryan Goodfellow <rgoodfel@isi.edu>
2017-11-23 20:15:34 -0800

    data linkage

--- api/restapi/handlers.go
+++ api/restapi/handlers.go
@@ -12,22 +12,48 @@ import (
 	"github.com/opus47/cloud/api/restapi/operations"
 )
 
+//TODO clearly this needs to hit a memcache and not mongo directly....
 func handleGetPiecesSearch(
 	params operations.GetPiecesSearchParams,
 ) middleware.Responder {
-	session, err := mgo.Dial("db")
+	session, err := mgo.Dial("172.17.0.2")
 	if err != nil {
 		log.Printf("dial error: %v", err)
 		return operations.NewGetPiecesSearchInternalServerError()
 	}
 	defer session.Close()
+
 	session.SetMode(mgo.Monotonic, true)
-	c := session.DB("opus47").C("pieces")
+	pc := session.DB("opus47").C("pieces")
 	result := []*models.Piece{}
-	err = c.Find(bson.M{}).All(&result)
+	err = pc.Find(bson.M{}).All(&result)
 	if err != nil {
 		log.Printf("find error: %v", err)
-		return operations.NewGetPiecesSearchOK().WithPayload(result)
+		return operations.NewGetPiecesSearchInternalServerError()
+	}
+
+	//resolve composers
+	cc := session.DB("opus47").C("composers")
+	for _, x := range result {
+		composer := models.Composer{}
+		err = cc.Find(bson.M{"@id": x.Composer}).One(&composer)
+		if err != nil {
+			log.Printf("find composer error: %s - %v", x.Composer, err)
+			return operations.NewGetPiecesSearchInternalServerError()
+		}
+		x.Composer = composer.Name
+	}
+
+	//resolve keys
+	kc := session.DB("opus47").C("keys")
+	for _, x := range result {
+		key := models.Key{}
+		err = kc.Find(bson.M{"@id": x.Key}).One(&key)
+		if err != nil {
+			log.Printf("find key error: %s - %v", x.Key, err)
+			return operations.NewGetPiecesSearchInternalServerError()
+		}
+		x.Key = key.Name
 	}
 
 	return operations.NewGetPiecesSearchOK().WithPayload(result)
--- db/launch_db.sh
+++ db/launch_db.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+docker kill opus47-db
 docker run -d --name opus47-db --rm  opus47/db:latest
